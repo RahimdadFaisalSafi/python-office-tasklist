6a5e672351bcbdd760391781d2663934245476cc
RahimdadFaisalSafi <rahimdadfaisal.safi@gmail.com>
2024-10-04 16:31:59 +0200

    Feature 2: Funktion zum Anzeigen der Aufgabenliste

--- python_office_tasklist.py
+++ python_office_tasklist.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 # Aufgabenliste anlegen
 tasks = []
 
@@ -15,3 +16,25 @@ def add_task():
         print(f"Die Aufgabe '{task}' wurde zur Liste hinzugefügt.")
 
 add_task()
+
+# Funktion zum Anzeigen der Aufgabenliste
+def show_tasklist():
+    if not tasks:
+        print("Deine Aufgabenliste ist leer.")
+    else:
+        print("Deine Aufgabenliste:")
+        today = datetime.today()
+
+        for task in tasks:
+            task_str = f"- {task['Aufgabe']}"
+            if "Fälligkeitsdatum" in task:
+                due_date = datetime.strptime(task["Fälligkeitsdatum"], "%Y-%m-%d")
+                task_str += f" (Fällig bis: {due_date.strftime('%Y-%m-%d')})"
+                
+                # Markiere Aufgaben in Rot, die innerhalb von 2 Tagen fällig sind
+                if due_date <= today + timedelta(days=2):
+                    task_str = f"\033[91m{task_str}\033[0m"  # ANSI Escape für rote Schrift
+            print(task_str)
+
+
+show_tasklist()
